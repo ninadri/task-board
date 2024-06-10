a3e8c94fd2fdfafa825edd9fecea6686c926a04d
ninadri <159666628+ninadri@users.noreply.github.com>
2024-06-09 21:04:57 -0500

    added usage for README

--- README.md
+++ README.md
@@ -1,22 +1,53 @@
-![when user first opens page](images/firstPage.png)
+# Task Board
 
+## User Story
 
-![when user clicks on Add task, a modal will appear](images/AddTitle.png)
+```md
+AS A project team member with multiple tasks to organize
+I WANT a task board
+SO THAT I can add individual project tasks, manage their state of progress and track overall project progress accordingly
+```
 
-![when user saves input, it will create a task card that is draggable and droppable](images/dragAndDrop.png)
+## Acceptance Criteria
 
+```md
+GIVEN a task board to manage a project
+WHEN I open the task board
+THEN the list of project tasks is displayed in columns representing the task progress state (Not Yet Started, In Progress, Completed)
+WHEN I view the task board for the project
+THEN each task is color coded to indicate whether it is nearing the deadline (yellow) or is overdue (red)
+WHEN I click on the button to define a new task
+THEN I can enter the title, description and deadline date for the new task into a modal dialog
+WHEN I click the save button for that task
+THEN the properties for that task are saved in localStorage
+WHEN I drag a task to a different progress column
+THEN the task's progress state is updated accordingly and will stay in the new column after refreshing
+WHEN I click the delete button for a task
+THEN the task is removed from the task board and will not be added back after refreshing
+WHEN I refresh the page
+THEN the saved tasks persist
+```
 
-I received help from Chipo in the AskLearn is Slack. The code he gave me that fixed mine is below
-" $('.lane').droppable({
-  accept: '.draggable',
-  drop: handleDrop 
-}); "
+## Usage
 
-" for (let task of tasks) {
-    if (task.id === parseInt(projectId)) {
-      task.status = newStatus;
-    }
-  }  "
+When the user selects 'Add Task,' they can input a task title, due date, and description. The task will then be displayed on the page, and users can drag it to the desired column: 'To Do,' 'In Progress,' or 'Done.'
 
-  https://github.com/ninadri/task-board/tree/main
-   https://ninadri.github.io/task-board/
+![when user first opens page](./images/firstPage.png)
+
+![when user clicks on Add task, a modal will appear](./images/enter-task.png)
+
+![when user saves input, it will create a task card that is draggable and droppable](./images/task-board-drag.png)
+
+When a task is overdue, it will display red.
+![when a task is overdue, it will display red](./images/overdue.png)
+
+When a task is nearing deadline, it will display yellow.
+![when a task is nearing deadline, it will display yellow](./images/due-date-near.png)
+
+## License
+
+MIT License
+
+---
+
+© Adriana Nino. [Github Repo](https://github.com/ninadri/task-board/tree/main). [Task Board Site](https://ninadri.github.io/task-board/).
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
-// Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 let nextId = JSON.parse(localStorage.getItem("nextId")) || 1;
-const taskNameInputEl = $('#task-name-input');
-const taskTypeInputEl = $('#task-description-input');
-const taskDateInputEl = $('#due-date-input');
-const taskFormEl = $('#project-form');
+const taskNameInputEl = $("#task-name-input");
+const taskTypeInputEl = $("#task-description-input");
+const taskDateInputEl = $("#due-date-input");
+const taskFormEl = $("#project-form");
 
 console.log("Retrieved taskList from localStorage:", taskList);
-// Todo: create a function to generate a unique task id
+// Generate a unique task id
 function generateTaskId() {
-    let newId = nextId++;
-    localStorage.setItem("nextId", JSON.stringify(nextId));
-    return newId;
+  let newId = nextId++;
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+  return newId;
 }
 
 function readTasksFromStorage() {
-
-  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
 
   if (!tasks) {
     tasks = [];
@@ -25,40 +23,35 @@ function readTasksFromStorage() {
 }
 
 function saveTasksToStorage(tasks) {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// Todo: create a function to create a task card
+// Creates a task card
 function createTaskCard(task) {
-  const taskCard = $('<div>')
-      .addClass('card project-card draggable my-3 task-card')
-      .attr('id', 'draggable-nonvalid')
-      .attr('data-project-id', task.id);
-  const cardHeader = $('<div>')
-      .addClass('card-header h4')
-      .text(task.name);
-  const cardBody = $('<div>')
-      .addClass('card-body');
-  const cardDescription = $('<p>')
-      .addClass('card-text').text(task.type);
-  const cardDueDate = $('<p>').addClass('card-text')
-      .text(task.dueDate);
-  const cardDeleteBtn = $('<button>')
-      .addClass('btn btn-danger delete')
-      .text('Delete')
-      .attr('data-project-id', task.id);
-  cardDeleteBtn.on('click', handleDeleteTask);
+  const taskCard = $("<div>")
+    .addClass("card project-card draggable my-3 task-card")
+    .attr("id", "draggable-nonvalid")
+    .attr("data-project-id", task.id);
+  const cardHeader = $("<div>").addClass("card-header h4").text(task.name);
+  const cardBody = $("<div>").addClass("card-body");
+  const cardDescription = $("<p>").addClass("card-text").text(task.type);
+  const cardDueDate = $("<p>").addClass("card-text").text(task.dueDate);
+  const cardDeleteBtn = $("<button>")
+    .addClass("btn btn-danger delete")
+    .text("Delete")
+    .attr("data-project-id", task.id);
+  cardDeleteBtn.on("click", handleDeleteTask);
 
-  if (task.dueDate && task.status !== 'done') {
+  if (task.dueDate && task.status !== "done") {
     const now = dayjs();
-    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+    const taskDueDate = dayjs(task.dueDate, "DD/MM/YYYY");
 
     // If the task is due today, make the card yellow. If it is overdue, make it red.
-    if (now.isSame(taskDueDate, 'day')) {
-      taskCard.addClass('bg-warning text-white');
+    if (now.isSame(taskDueDate, "day")) {
+      taskCard.addClass("bg-warning text-white");
     } else if (now.isAfter(taskDueDate)) {
-      taskCard.addClass('bg-danger text-white');
-      cardDeleteBtn.addClass('border-light');
+      taskCard.addClass("bg-danger text-white");
+      cardDeleteBtn.addClass("border-light");
     }
   }
 
@@ -68,77 +61,77 @@ function createTaskCard(task) {
   return taskCard;
 }
 
-
-// // Todo: create a function to render the task list and make cards draggable
- function renderTaskList() {
+// Renders the task list and make cards draggable
+function renderTaskList() {
   const tasks = readTasksFromStorage();
 
-const todoList = $('#not-started-tasks');
- todoList.empty();
+  const todoList = $("#not-started-tasks");
+  todoList.empty();
 
- const inProgressList = $('#in-progress-tasks');
- inProgressList.empty();
+  const inProgressList = $("#in-progress-tasks");
+  inProgressList.empty();
 
- const doneList = $('#completed-tasks');
- doneList.empty();
+  const doneList = $("#completed-tasks");
+  doneList.empty();
 
- todoList.append('<h3 class="text-center">To Do</h3>'); 
- inProgressList.append('<h3 class="text-center">In Progress</h3>'); 
- doneList.append('<h3 class="text-center">Done</h3>');
+  todoList.append('<h3 class="text-center">To Do</h3>');
+  inProgressList.append('<h3 class="text-center">In Progress</h3>');
+  doneList.append('<h3 class="text-center">Done</h3>');
 
-
- for (let task of tasks) {
-   if (task.status === 'to-do') {
-     todoList.append(createTaskCard(task));
-   } else if (task.status === 'in-progress-tasks') {
-    inProgressList.append(createTaskCard(task));
-   } else if (task.status === 'completed-tasks') {
-     doneList.append(createTaskCard(task));
-   }
- }
-
-    $('.draggable').draggable({
-      opacity: 0.7,
-      zIndex: 100,
-      helper: function (e) {
-        const original = $(e.target).hasClass('ui-draggable')
-          ? $(e.target)
-          : $(e.target).closest('.ui-draggable');
-        return original.clone().css({
-         width: original.outerWidth(),
-     });
-    },
-    });
+  for (let task of tasks) {
+    if (task.status === "to-do") {
+      todoList.append(createTaskCard(task));
+    } else if (task.status === "in-progress-tasks") {
+      inProgressList.append(createTaskCard(task));
+    } else if (task.status === "completed-tasks") {
+      doneList.append(createTaskCard(task));
+    }
   }
 
-// Todo: create a function to handle deleting a task
+  $(".draggable").draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    helper: function (e) {
+      const original = $(e.target).hasClass("ui-draggable")
+        ? $(e.target)
+        : $(e.target).closest(".ui-draggable");
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
+}
+
+// Deletes a task
 function handleDeleteTask() {
-  const projectId = parseInt($(this).attr('data-project-id'), 10);
+  const projectId = parseInt($(this).attr("data-project-id"), 10);
   const tasks = readTasksFromStorage();
 
-  for (let i = 0; i < tasks.length; i++) { 
-    if (tasks[i].id === projectId) { 
-      tasks.splice(i, 1); break; }}
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === projectId) {
+      tasks.splice(i, 1);
+      break;
+    }
+  }
 
-        
   saveTasksToStorage(tasks);
   renderTaskList();
 }
 
-// Todo: create a function to handle adding a new task
+// Adds a new task
 function handleAddTask(event) {
   event.preventDefault();
 
   const taskName = taskNameInputEl.val().trim();
-  const taskType = taskTypeInputEl.val(); 
+  const taskType = taskTypeInputEl.val();
   const taskDate = taskDateInputEl.val();
 
-  const newTask = { 
-      id: generateTaskId(),
-      name: taskName,
-      type: taskType,
-      dueDate: taskDate,
-      status: 'to-do'
+  const newTask = {
+    id: generateTaskId(),
+    name: taskName,
+    type: taskType,
+    dueDate: taskDate,
+    status: "to-do",
   };
 
   const tasks = readTasksFromStorage();
@@ -147,14 +140,14 @@ function handleAddTask(event) {
   saveTasksToStorage(tasks);
   renderTaskList();
 
-  taskNameInputEl.val('');
-  taskTypeInputEl.val('');
-  taskDateInputEl.val('');
+  taskNameInputEl.val("");
+  taskTypeInputEl.val("");
+  taskDateInputEl.val("");
 
-  $('#exampleModal').modal('hide');
+  $("#exampleModal").modal("hide");
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+// Drops a task into a new status lane
 function handleDrop(event, ui) {
   const tasks = readTasksFromStorage();
 
@@ -165,42 +158,43 @@ function handleDrop(event, ui) {
     if (task.id === parseInt(projectId)) {
       task.status = newStatus;
     }
+  }
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTaskList();
 }
 
-localStorage.setItem('tasks', JSON.stringify(tasks));
-renderTaskList();
-}
+taskFormEl.on("submit", handleAddTask);
+taskFormEl.on("click", ".btn-delete-project", handleDeleteTask);
 
-taskFormEl.on('submit', handleAddTask);
-taskFormEl.on('click', '.btn-delete-project', handleDeleteTask);
-
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// Renders the task list
 
 $(document).ready(function () {
   renderTaskList();
- 
-  $('.droppable').droppable ({
+
+  $(".droppable").droppable({
     accept: ".draggable",
     classes: {
       "ui-droppable-active": "ui-state-active",
-      "ui-droppable-hover": "ui-state-hover"
+      "ui-droppable-hover": "ui-state-hover",
     },
-    drop: function(event, ui) {
-      $(this)
-        .find("taskFormEl")
-    }
+    drop: function (event, ui) {
+      $(this).find("taskFormEl");
+    },
   });
 });
 
-$('#due-date-input').datepicker(); 
+// makes the due date field a date picker
+$("#due-date-input").datepicker();
 
-$('.lane').droppable({
-  accept: '.draggable',
-  drop: handleDrop
+// makes lanes droppable
+$(".lane").droppable({
+  accept: ".draggable",
+  drop: handleDrop,
 });
 
-$('#save-task').on('click', handleAddTask);
+$("#save-task").on("click", handleAddTask);
 
-window.addEventListener('beforeunload', (event) => {
+window.addEventListener("beforeunload", (event) => {
   localStorage.clear();
 });
